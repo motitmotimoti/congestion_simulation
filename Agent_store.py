145cc1c70090c6d812b2ad8445cd4bfc89efcd84
Seiichi Mochida <m1410141kskb38@gmail.com>
2019-06-27 16:45:52 +0900

    add show_store_info()

--- Agent_store.py
+++ Agent_store.py
@@ -5,7 +5,7 @@ class Agent_store:
     def __init__(self):
         """
         type = [Groceries:Gr, Clothes:Cl, Restaurant:Rt, Cafe:Cf, Miscellaneous goods:Mg]
-        storage: number of people store can Accommodate
+        storage: number of people store can accommodate
         instore[]: agent_id in store
         waiting[]: number of people waiting
         visitor[]: store visiting people each season
@@ -32,16 +32,25 @@ def init_agent_store(agents_store):
     for store_id, agent_store in enumerate(agents_store):
         if store_id == 0:
             agent_store.type = 'Gr'
-            agent_store.storage = 100000
+            agent_store.capacity = 100000
         elif store_id == 1:
             agent_store.type = 'Cl'
-            agent_store.storage = 100000
+            agent_store.capacity = 100000
         elif store_id == 2:
             agent_store.type = 'Rt'
-            agent_store.storage = 80
+            agent_store.capacity = 4
         elif store_id == 3:
             agent_store.type = 'Cf'
-            agent_store.storage = 40
+            agent_store.capacity = 10
         else:
             agent_store.type = 'Mg'
-            agent_store.storage = 100000
+            agent_store.capacity = 100000
+
+
+def show_store_info(agents_store):
+    """
+    show agents_store infomation
+    """
+
+    for agent_store in agents_store:
+        print(f'type:{agent_store.type}, capa:{agent_store.capacity}, instore:{agent_store.instore}, waiting:{agent_store.waiting}, visitor:{agent_store.visitor}')
