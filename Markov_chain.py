fff7747f27b5e342ed81dc4bae7c070df935e6de
Seiichi Mochida <m1410141kskb38@gmail.com>
2019-07-26 19:49:59 +0900

    割合ではなく個数での表示に変更

--- Markov_chain.py
+++ Markov_chain.py
@@ -4,21 +4,22 @@ import numpy as np
 
 #result_list_state.csvの読み込み
 #ディレクトリの変更をするように！！
-df_state = pd.read_csv('result/high/2019-07-10/result_list_state_high.csv', index_col=0)
+df_state = pd.read_csv('result/high/2019-07-12/result_list_state_3.csv', index_col=0)
 df_state = df_state.fillna('Na')
 length = len(df_state)
 
 
 #result_info.csvの読み込み
 #ディレクトリの変更を忘れずに！！！
-df_info = pd.read_csv('result/high/2019-07-10/result_info_high.csv',index_col=0)
+df_info = pd.read_csv('result/high/2019-07-12/result_info_3.csv',index_col=0)
 df_info_init = df_info.iloc[:1,0:6]
 
 df_info_0 = df_info.iloc[:,7]
 df_info_0 = df_info_0.values.tolist()
 #print(f'df_info_init:\n{df_info_init}')
 state = df_info_init.values
-state_n = state / df_info_0[1] #初期状態
+#state_n = state / df_info_0[1] #初期状態
+state_n = state #初期状態
 print(f'init_state:\n{state}')
 print(f'init_state_n:\n{state_n}')
 #print(f'df_info_init:\n{df_info_init}')
@@ -30,7 +31,7 @@ for x in range(0,length-1):
     print('########################')
     mat_x, result_x = analyze.generate_matrix(df_state, df_info,state_n, x)
     #ディレクトリは生成し、変更すること
-    #np.save(f'matrix/high/2019-07-10/result_test_{x+1}',result_x)
+    np.save(f'matrix/high/2019-07-12/matrix_3/result_matrix_{x+1}',result_x)
     print(f'result_{x+1}:\n{result_x}')
     print(f'mat_{x+1}:\n{mat_x}')
     state_n = np.dot(state_n,result_x)
@@ -43,6 +44,6 @@ for x in range(0,length-1):
 #print(result_markov)
 markov = pd.DataFrame(result_markov, columns=['Gr','Cl','Rt','Cf','Mg','0_out'])
 #ディレクトリの変更
-markov.to_csv(f'markov/high/2019-07-10/markov_chain.csv')
+markov.to_csv(f'markov/high/2019-07-12/markov_chain_3.csv')
 
 #print(np.load('matrix/high/2019-07-10/result_test_1.npy'))
