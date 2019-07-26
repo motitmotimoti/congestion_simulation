40e2010c23a21c73fe703c51e4e711d842d02ed9
Seiichi Mochida <m1410141kskb38@gmail.com>
2019-07-26 19:50:22 +0900

    割合ではなく個数での表示に変更

--- Markov_analyze.py
+++ Markov_analyze.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 #シーズン数の入力
 season = 36
+#season = 3
 
 #初期状態の入力
 #result_info.csvの読み込み
-#ディレクトリの変更を忘れずに！！！
-df_info = pd.read_csv('result_info.csv',index_col=0)
+#=================ディレクトリの変更を忘れずに！！！
+df_info = pd.read_csv('result/low/2019-07-12/result_info_1.csv',index_col=0)
 df_info_init = df_info.iloc[:1,0:6]
 
 df_info_0 = df_info.iloc[:,7]
@@ -20,17 +21,24 @@ state = df_info_init.values
 print(f'new:{new[1]}')
 print(f'df_info_0:{df_info_0[1]}')
 state[0,5] = new[1]
+#state[0,5] = 0
 #print(f'dist:{dist}')
 print(f'state:{state}')
 
 
-state_n = state / df_info_0[1] #初期状態
+#state_n = state / df_info_0[1] #初期状態
+state_n = state #初期状態
 print(f'state_init:{state_n}')
 
 result_markov = []
 
 for x in range(1,season):
-    matrix = np.load(f'matrix/high/2019-07-10/result_test_{x}.npy')
+    if x != 1:
+        #state_n[0,5] = float( new[x] / (df_info_0[x-1] + new[x]) )
+        state_n[0,5] = new[x]
+    print(f'state_n:\n{state_n}')
+    #======================ディレクトリの変更忘れずに！！！
+    matrix = np.load(f'matrix/low/2019-07-12/matrix_2/result_matrix_{x}.npy')
     print(f'matrix:{matrix}')
     state_n = np.dot(state_n,matrix)
     print(f'{x}シーズン')
@@ -40,7 +48,7 @@ for x in range(1,season):
     result_markov.append(state_m[0].tolist())
 
 #print(result_markov)
-#matrixで使った尺度のディレクトリに保存
-#ファイル名は(用いた初期状態)_(用いたmatrix)_markov_analyze.csv
+#=================matrixで使った尺度のディレクトリに保存
+#=================ファイル名は(用いた初期状態)_(用いたmatrix)_markov_analyze.csv
 markov = pd.DataFrame(result_markov, columns=['Gr','Cl','Rt','Cf','Mg','0_out'])
-markov.to_csv(f'analyze/high/test_high_markov_analyze.csv')
+markov.to_csv(f'analyze/low/2019-07-12/low1_low2_markov_analyze.csv')
