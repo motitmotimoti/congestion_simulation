50e8c8acfa1968703a3d59a4a35b75d74ee38bc6
Seiichi Mochida <m1410141kskb38@gmail.com>
2019-07-29 01:40:04 +0900

    new_generate_agents()の修正

--- Agent_man.py
+++ Agent_man.py
@@ -33,16 +33,16 @@ def generate_agents(num_agent):
     agents = [Agent_man() for agent_id in range(num_agent)]
     return agents
 
-def new_generate_agents(num_new_agent,agents):
+def new_generate_agents(num_new_agent,agents,agents_store, time,length):
     """
     new agents generate in Move section
     """
     #num_new_agent = rnd.randrange(num_new_generate)
     new_agents = [Agent_man() for new_agent_id in range(num_new_agent)]
-    Move.state_task_for_new_agents(new_agents)
     Decision.initial_strategy(new_agents)
+    Move.state_task_for_new_agents(new_agents,agents_store,time,length)
     for id,agent in enumerate(new_agents):
-        print(f'new_agents No.{id}, state:{agent.state}, next_state:{agent.next_state}, strategy:{agent.strategy}, next_strategy:{agent.next_strategy}, task:{agent.task}')
+        print(f'new_agents No.{id}, state:{agent.state}, next_state:{agent.next_state}, strategy:{agent.strategy}, next_strategy:{agent.next_strategy}, task:{agent.task - 1}')
     agents.extend(new_agents)
 
 
