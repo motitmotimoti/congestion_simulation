81e51b38ffb16e62e701971fe68ccec2f91ee152
Seiichi Mochida <m1410141kskb38@gmail.com>
2019-07-29 01:42:24 +0900

    デバックのコメント化

--- Manage_store.py
+++ Manage_store.py
@@ -13,7 +13,7 @@ def divide_visitor(agents):
     list_Cf = [agent_id for agent_id, agent in enumerate(agents) if agent.state == 'Cf']
     list_Mg = [agent_id for agent_id, agent in enumerate(agents) if agent.state == 'Mg']
 
-    print(f'list_Gr:{list_Gr}\n list_Cl:{list_Cl}\n list_Rt:{list_Rt}\n list_Cf:{list_Cf}\n list_Mg:{list_Mg}\n')
+    #print(f'list_Gr:{list_Gr}\n list_Cl:{list_Cl}\n list_Rt:{list_Rt}\n list_Cf:{list_Cf}\n list_Mg:{list_Mg}\n')
 
     rnd.shuffle(list_Gr)
     rnd.shuffle(list_Cl)
@@ -21,7 +21,7 @@ def divide_visitor(agents):
     rnd.shuffle(list_Cf)
     rnd.shuffle(list_Mg)
 
-    print(f'list_Gr:{list_Gr}\n list_Cl:{list_Cl}\n list_Rt:{list_Rt}\n list_Cf:{list_Cf}\n list_Mg:{list_Mg}\n')
+    #print(f'list_Gr:{list_Gr}\n list_Cl:{list_Cl}\n list_Rt:{list_Rt}\n list_Cf:{list_Cf}\n list_Mg:{list_Mg}\n')
 
     return list_Gr, list_Cl, list_Rt, list_Cf, list_Mg
 
@@ -32,10 +32,10 @@ def manage_instore_waiting_first(agents,agents_store,list,type):
 
     for agent_store in agents_store:
         if agent_store.type == type:
-            print(f'type:{type}')
+            #print(f'type:{type}')
             tmp = agent_store.instore + list
-            print(f'tmp:{tmp}')
-            print(f'capa:{agent_store.capacity}')
+            #print(f'tmp:{tmp}')
+            #print(f'capa:{agent_store.capacity}')
             agent_store.instore, agent_store.waiting = tmp[:agent_store.capacity], tmp[agent_store.capacity:]
             print(f'instore:{agent_store.instore}, waiting:{agent_store.waiting}')
             for id, agent in enumerate(agents):
@@ -55,12 +55,12 @@ def manage_instore_waiting_second(agents,agents_store,list_x,type):
 
     for agent_store in agents_store:
         if agent_store.type == type:
-            print(f'type:{type}')
+            #print(f'type:{type}')
             tmp = agent_store.instore + agent_store.waiting + list_x
-            print(f'tmp:{tmp}')
-            print(f'capa:{agent_store.capacity}')
+            #print(f'tmp:{tmp}')
+            #print(f'capa:{agent_store.capacity}')
             tmp = list(dict.fromkeys(tmp))
-            print(f'tmp:{tmp}')
+            #print(f'tmp:{tmp}')
             agent_store.instore, agent_store.waiting = tmp[:agent_store.capacity], tmp[agent_store.capacity:]
             print(f'instore:{agent_store.instore}, waiting:{agent_store.waiting}')
 
